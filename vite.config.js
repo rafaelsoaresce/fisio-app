74ab795c939ebc1b5dd9b8c93ef0f207b3669b84
rafael.soares <rafael.soares@objective.com.br>
2025-05-26 21:41:52 -0300

    Transformando em um PWA

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,34 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.svg', 'masked-icon.svg'],
+      manifest: {
+        name: 'Rocket Fisio App',
+        short_name: 'Rocket Fisio App',
+        description: 'Aplicativo de fisioterapia',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'pwa-192x192.svg',
+            sizes: '192x192',
+            type: 'image/svg'
+          },
+          {
+            src: 'pwa-512x512.svg',
+            sizes: '512x512',
+            type: 'image/svg'
+          }
+        ]
+      }
+    })
   ],
 })
